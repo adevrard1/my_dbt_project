33f9b59e9f099580bc724edbbbac2d4856b0e286
Adele Evrard <adele.evrard.l@gmail.com>
2026-04-06 18:23:25 +0000

    v7

--- models/staging/local_bike_sales/stg_local_bike_sales__order_items.sql
+++ models/staging/local_bike_sales/stg_local_bike_sales__order_items.sql
@@ -1,8 +1,9 @@
 SELECT
+  cast(concat(order_id,'_',order_item) as string) as order_item_id,
   cast(order_id as string) as order_id,
   cast(item_id as string) as item_id,
   cast(product_id as string) as product_id,
-  round(quantity * (list_price - (list_price * discount)),2) as item_amount,
+  round(quantity * (list_price - (list_price * discount)),2) as order_item_amount,
   quantity as item_quantity,
   list_price as std_price,
   round(list_price - (list_price * discount),2) as reduced_price,
