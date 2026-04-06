8a96a2ca7d2aa921b112e3d4ef5ad6a6355f5eb1
Adele Evrard <adele.evrard.l@gmail.com>
2026-04-06 20:20:52 +0000

    v13

---
+++ models/intermediate/local_bike/int_local_bike__orders.sql
@@ -0,0 +1,58 @@
+with order_sales as (
+
+select 
+    order_id, 
+    sum(order_item_amount) as total_order_amount,
+    sum(order_item_quantity) as total_order_quantity,
+    count(distinct item_id) as total_distinct_items
+from dbt_aevrard_stg_local_bike_sales.stg_local_bike_sales__order_items
+group by order_id
+
+), customers_details as (
+
+select 
+    customer_id,
+    city as customer_city,
+    state as customer_state
+from dbt_aevrard_stg_local_bike_sales.stg_local_bike_sales__customers
+
+), staffs_details as (
+
+select
+    staff_id,
+    concat(first_name,'_',last_name) as staff_name
+from dbt_aevrard_stg_local_bike_sales.stg_local_bike_sales__staffs
+
+), stores_details as (
+
+select 
+    store_id,
+    city as store_city,
+    state as store_state
+from dbt_aevrard_stg_local_bike_sales.stg_local_bike_sales__stores
+)
+
+select 
+    order_id,
+    customer_id,
+    customer_state,
+    customer_city,
+    staff_id,
+    staff_name,
+    store_id,
+    store_name,
+    store_city,
+    store_state,
+    manager_id,
+    order_status,
+    required_date,
+    shipped_date,
+    total_order_amount,
+    total_order_quantity,
+    total_distinct_items
+
+from dbt_aevrard_stg_local_bike_sales.stg_local_bike_sales__orders as o
+left join order_sales os on o.order_id = os.order_id 
+left join customers_details cd on o.customer_id = cd.customer_id
+left join staffs_details sd on sd.staff_id = o.staff_id
+left join stores_details std on std.store_id = o.store_id
