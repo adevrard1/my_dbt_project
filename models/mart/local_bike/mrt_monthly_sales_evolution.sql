5177abae34164bdc38490b6efcd7e54e523f2e9e
Adele Evrard <adele.evrard.l@gmail.com>
2026-04-07 11:14:41 +0000

    v16

--- models/mart/local_bike/mrt_monthly_sales_evolution.sql
+++ models/mart/local_bike/mrt_monthly_sales_evolution.sql
@@ -7,19 +7,19 @@ WITH
         SELECT
           DATE_TRUNC(MIN(order_date), MONTH) AS min_date,
           DATE_TRUNC(MAX(order_date), MONTH) AS max_date
-        FROM `databird-488418.dbt_aevrard_int_local_bike.int_local_bike__orders`
+        FROM {{ref('int_local_bike__orders')}}
       ),
       UNNEST(
         GENERATE_DATE_ARRAY(min_date, max_date, INTERVAL 1 MONTH)) AS month_year
   ),
   monthly_sales AS (
     SELECT
-      DATE_TRUNC(order_date, MONTH) AS month_year,
-      ROUND(SUM(total_order_amount), 2) AS total_sales_amount,
-      SUM(total_order_quantity) AS total_sales_quantity,
-      COUNT(DISTINCT order_id) AS nb_orders,
-      SAFE_DIVIDE(SUM(total_order_amount),COUNT(DISTINCT order_id)) as avg_order_value
-    FROM `databird-488418.dbt_aevrard_int_local_bike.int_local_bike__orders`
+    DATE_TRUNC(order_date, MONTH) AS month_year,
+    ROUND(SUM(total_order_amount), 2) AS total_sales_amount,
+    SUM(total_order_quantity) AS total_sales_quantity,
+    COUNT(DISTINCT order_id) AS nb_orders,
+    ROUND(SAFE_DIVIDE(SUM(total_order_amount), COUNT(DISTINCT order_id)),2) AS avg_order_value
+    FROM {{ref('int_local_bike__orders')}}
     GROUP BY
       DATE_TRUNC(order_date, MONTH)
   ),
@@ -53,19 +53,19 @@ WITH
 
 SELECT
   c.month_year,
-  total_sales_amount,
-  precedent_month_total_sales_amount,
-  lastyear_month_total_sales_amount,
-  total_sales_quantity,
-  precedent_month_total_sales_quantity,
-  lastyear_month_total_sales_quantity,
-  nb_orders,
-  precedent_month_nb_orders,
-  lastyear_month_nb_orders,
-  avg_order_value,
-  precedent_month_avg_order_value,
-  lastyear_month_avg_order_value
-
+  COALESCE(total_sales_amount,0) as total_sales_amount,
+  COALESCE(ROUND((SAFE_DIVIDE(total_sales_amount,precedent_month_total_sales_amount)-1)*100,1),0) as evol_sales_amount_vs_precedent_month,  
+  COALESCE(ROUND((SAFE_DIVIDE(total_sales_amount,lastyear_month_total_sales_amount)-1)*100,1),0) as evol_sales_amount_vs_lastyear,
+  COALESCE(total_sales_quantity,0) as total_sales_quantity,
+  COALESCE(ROUND((SAFE_DIVIDE(total_sales_quantity,precedent_month_total_sales_quantity)-1)*100,1),0) as evol_sales_quantity_vs_precedent_month,  
+  COALESCE(ROUND((SAFE_DIVIDE(total_sales_quantity,lastyear_month_total_sales_quantity)-1)*100,1),0) as evol_sales_quantity_vs_lastyear,
+  COALESCE(nb_orders,0) as nb_orders,
+  COALESCE(ROUND((SAFE_DIVIDE(total_sales_quantity,precedent_month_nb_orders)-1)*100,1),0) as evol_nb_orders_vs_precedent_month,  
+  COALESCE(ROUND((SAFE_DIVIDE(nb_orders,lastyear_month_nb_orders)-1)*100,1),0) as evol_nb_orders_vs_lastyear,
+  COALESCE(avg_order_value,0) as avg_order_value,
+  COALESCE(ROUND((SAFE_DIVIDE(avg_order_value,precedent_month_avg_order_value)-1)*100,1),0) as evol_avg_order_value_vs_precedent_month,  
+  COALESCE(ROUND((SAFE_DIVIDE(avg_order_value,lastyear_month_avg_order_value)-1)*100,1),0) as evol_avg_order_value_vs_lastyear
+  
 FROM calendar c
 LEFT JOIN monthly_sales ms ON c.month_year = ms.month_year
 LEFT JOIN precedent_monthly_sales pms ON c.month_year = pms.month_year
