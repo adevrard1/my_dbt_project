d00e8694c0a1ea525a78762414943ec2b81370db
Adele Evrard <adele.evrard.l@gmail.com>
2026-04-07 19:55:20 +0000

    last

--- models/intermediate/local_bike/int_local_bike__stocks.sql
+++ models/intermediate/local_bike/int_local_bike__stocks.sql
@@ -5,12 +5,6 @@ SELECT
   s.product_id,
   product_name,
   stock_to_date
--- avg_daily_sales_last_30d,
--- estimated_nb_days_before_rupture,
--- estimated_rupture_date,
--- status_of_stock,
--- nb_orders_waiting,
--- nb_stock_to_date_vs_nb_orders_waiting
 FROM {{ref('stg_local_bike_production__stocks')}} s 
 LEFT JOIN {{ref('stg_local_bike_sales__stores')}} st
   ON s.store_id = st.store_id
