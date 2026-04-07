4b9a4f9154dd3b302e04ace4761590f1a97ece3f
Adele Evrard <adele.evrard.l@gmail.com>
2026-04-07 18:38:21 +0000

    modif stg

--- models/staging/local_bike_production/stg_local_bike_production__products.sql
+++ models/staging/local_bike_production/stg_local_bike_production__products.sql
@@ -4,5 +4,5 @@ SELECT
   brand_id,
   category_id,
   extract (year from cast(concat(model_year, '-01-01') as date)) as model_year,
-  list_price as std_price
+  list_price as start_price
 FROM {{source('local_bike','products')}}
